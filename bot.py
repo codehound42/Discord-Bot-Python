e9f0eabcfaf57a582c20ddec6851723fa3a92111
codehound42 <41467365+codehound42@users.noreply.github.com>
2020-08-01 20:43:30 +0200

    Add error handling and access control

--- bot.py
+++ bot.py
@@ -1,11 +1,21 @@
 import discord
 from discord.ext import commands
 import keys
+from datetime import datetime
+import random
+
+###########################
+# Globals
+###########################
 
 client = discord.Client()
 client = commands.Bot(command_prefix='!')
 is_client_running = False
 
+
+###########################
+# Events
+###########################
 
 @client.event
 async def on_ready():
@@ -13,11 +23,7 @@ async def on_ready():
 
     if not is_client_running:
         is_client_running = True
-        await init()
-
-
-async def init():
-    print(f"Bot {client.user.name} initialising...")
+        print(f"Bot {client.user.name} initialising...")
 
 
 @client.event
@@ -31,22 +37,74 @@ async def on_message(message):
         await message.channel.send(f"Hello {message.author.mention}")
         return
     
-    # Process command
     if not message.content.startswith('!'):
         return
+        
+    # Process command
     await message.channel.trigger_typing()
     await client.process_commands(message)
 
 
+@client.event
+async def on_command_error(ctx, error):
+    await ctx.send(error)
+    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "command exception", type(error), error)
+
+
+###########################
+# Auxiliary Functions
+###########################
+
+def check_if_lucky(chance=0.5):
+    def predicate(ctx):
+        return random.random() < chance
+    return commands.check(predicate)
+
+
+###########################
+# Commands
+###########################
+
 @client.command()
+@commands.dm_only()
 async def ping(ctx):
     await ctx.send("pong!")
 
 
+@client.command()
+@commands.guild_only()
+async def ding(ctx):
+    await ctx.send("dong!")
+
+
+@client.command()
+@commands.has_permissions(manage_webhooks=True)
+async def iBeDev(ctx):
+    await ctx.send("you be dev")
+
+
+@client.command()
+@commands.has_any_role("admin", "moderator")
+async def randomRange(ctx, a, b):
+    result = random.randrange(int(a), int(b))
+    await ctx.send(f"Random int in range [{a}; {b}[: **{result}**")
+
+
+@client.command()
+@check_if_lucky()
+async def tryMyLuck(ctx):
+    await ctx.send("You're lucky pal!!!")
+
+
 @client.command(aliases=['youHaveBeenTerminated'])
+@commands.is_owner()
 async def terminate(ctx):
     await ctx.send("Terminating...")
     await client.logout()
 
+
+###########################
+# Run Client
+###########################
 
 client.run(keys.TOKEN)
